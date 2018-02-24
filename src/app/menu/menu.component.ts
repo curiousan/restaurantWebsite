32ec7bda64f937be5666801d4e1c2ac84e82c22f
Sandesh Poudel <curious.sandesh@gmail.com>
2018-02-24 13:24:24 +0200

    Basic translation and menu items done

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Menu } from './menu';
 
 @Component({
   selector: 'app-menu',
@@ -11,5 +12,51 @@ export class MenuComponent implements OnInit {
 
   ngOnInit() {
   }
+  
+  selctMenu(id : Number) : void {
+
+  }
+
+  menu : [Menu] = [{
+    id: 2,
+    name: 'Momo',
+    image: 'momo.png',
+    desc: 'Most loved nepali food',
+    price: 15,
+    ingredient: ['flour','soup','chicken'],
+    glutFree: true,
+    lactFree: true
+},
+{
+  id: 3,
+  name: 'Nepali Khana',
+  image: 'Nepali-Khana.png',
+  desc: 'Typical nepali bread',
+  price: 18,
+  ingredient: ['Rice','bread','meat','soup'],
+  glutFree: true,
+  lactFree: false
+},
+{
+  id: 4,
+  name: 'Cheese Naan',
+  image: 'cheese-naaan.png',
+  desc: 'Typical nepali bread',
+  price: 16,
+  ingredient: ['pea','chilly','mustard'],
+  glutFree: true,
+  lactFree: false
+},
+{
+  id: 5,
+  name: 'Newari Khaja',
+  image: 'newari-food.png',
+  desc: 'Typical newari breakfast',
+  price: 16,
+  ingredient: ['pea','chilly','mustard'],
+  glutFree: true,
+  lactFree: false
+}
+];
 
 }
---
+++ src/app/menu/menu.ts
@@ -0,0 +1,10 @@
+export class Menu {
+    id: number;
+    name: String;
+    image: String;
+    desc: String;
+    price: number;
+    ingredient: [String];
+    glutFree : boolean;
+    lactFree : boolean;
+}
