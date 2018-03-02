dbafcda46cd3775b0e38b54b458ed63fdf29bb4b
Sandesh Poudel <curious.sandesh@gmail.com>
2018-03-02 21:45:44 +0200

    merge with days basis launh menu

--- src/app/web.service.ts
+++ src/app/web.service.ts
@@ -10,7 +10,7 @@ export class WebService {
   CLIENT_ID = '5029400431.347fa4c.4a9b17a36312417e8c4643bfbcf511db';
   USER_ID = '5029400431';
   ACCESS_TOKEN = '5029400431.347fa4c.4a9b17a36312417e8c4643bfbcf511db';
-  imagesUrl: any[] = [];
+  imagesUrl: string[] = [];
 
   constructor(private http: HttpClient) {
     console.log('web service initialized');
@@ -29,10 +29,8 @@ export class WebService {
      // tslint:disable-next-line:no-unused-expression
 
     this.http.get('https://api.instagram.com/v1/users/self/media/recent/?access_token='+this.ACCESS_TOKEN+'&count=2').subscribe(data =>{
-      data.data.forEach(element => {
-        this.imagesUrl.push(element.images.low_resolution.url);
-      });
-     },
+      console.log(data.json());
+       },
     err => {
       console.log('Error');
     });
